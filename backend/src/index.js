65df2abec185bdab515be2b348d792cf75a10461
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2026-01-05 19:50:25 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
+app.set("trust proxy", 1);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken =(userID,res)=>{
      res.cookie("jwt",token,{
         maxAge: 7 *24*60*60*1000, //MS
         httpOnly:true,//prevent XSS attacks cross-site Scripting attacks
-        sameSite:"strict", //CSRF attacks cross-site request forgery attacts
+        sameSite:"none", //CSRF attacks cross-site request forgery attacts
         secure:process.env.NODE_ENV !=='development'
      })
      return token;
