97d63379aab2f46c30c814a9f4260c9bc8b25f09
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2026-01-05 18:49:00 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,19 +5,18 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connectDb } from "./lib/db.js";
-import { app,server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
 
-
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chatty-murex-omega.vercel.app"],
     credentials: true,
   })
 );
