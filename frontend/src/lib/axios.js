afd3708e5930f7f6b5af4dd4c43464f99af99d78
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2026-01-05 19:25:33 +0530

    ready for connecting to backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,13 +1,15 @@
 import axios from "axios"
 
-export const axiosInstance =axios.create({
-    baseURL:"http://localhost:5001/api",
-    withCredentials:true,
+export const axiosInstance = axios.create({
+    // Replace localhost with your RENDER URL
+    baseURL: "https://backend-chatty-da8f.onrender.com/api", 
+    withCredentials: true,
 });
+
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem("auth-token");
     if (token) {
       config.headers["auth-token"] = token;
     }
     return config;
-  });
+});
