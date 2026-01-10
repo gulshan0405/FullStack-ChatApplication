afb4f896443d166c0148f61a1064d53dec6d7ab9
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2026-01-10 16:42:29 +0530

    backend fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,18 +31,24 @@ app.use(
       "https://chatty-murex-omega.vercel.app",
       "https://chatty-ascsfqtxt-gulshan0405s-projects.vercel.app"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
+
 
 
 
